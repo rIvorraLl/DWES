1a6d3348384d1e3968665b75cbf6ec994bd6ec26
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2022-10-01 23:28:12 +0200

    add: root index.php, changed: styles, links, more

---
+++ C02PG01/js/scripts.js
@@ -0,0 +1,5 @@
+function back() {
+    window.open("/index.php");
+}
+
+
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+    --- Exercise C02PG01 ---
+    Print a matrix with the output of different
+    functions with different variable values.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css_root/styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>EJERCICIOS DE LA ASIGNATURA:</h1>
+            <h1>DESARROLLO WEB EN ENTORNO SERVIDOR</h1>
+            <h3>Curso: 2022-2023</h3>
+        </header>
+        <main>
+            <div>
+            <a href="C02PG01/index.php">Ejercicio 1: crear una matriz de comprobación de diferentes pares función/valor</a>
+            </div>
+        </main>
+    </body>
+</html>
